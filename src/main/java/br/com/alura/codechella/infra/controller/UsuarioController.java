3573b08565405e3d9091205eebf1b5800e610152
Mateus Burlamaqui Moreira <mateusblm@outlook.com>
2025-09-22 15:20:02 -0300

    feat: implementa controlador e DTO para cadastro de usuário

---
+++ src/main/java/br/com/alura/codechella/infra/controller/UsuarioController.java
@@ -0,0 +1,26 @@
+package br.com.alura.codechella.infra.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.codechella.application.usecases.CadastrarUsuario;
+import br.com.alura.codechella.domain.entities.usuario.Usuario;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+    
+    private final CadastrarUsuario cadastrarUsuario;
+
+    public UsuarioController(CadastrarUsuario cadastrarUsuario) {
+        this.cadastrarUsuario = cadastrarUsuario;
+    }
+
+    @PostMapping
+    public UsuarioDto cadastrarUsuario(@RequestBody UsuarioDto dto){
+        Usuario salvo = cadastrarUsuario.cadastrarUsuario(new Usuario(dto.cpf(), dto.nome(), dto.nascimento(), dto.email()));
+        return new UsuarioDto(salvo.getCpf(), salvo.getNome(), salvo.getNascimento(), salvo.getEmail());
+    }
+}
---
+++ src/main/java/br/com/alura/codechella/infra/controller/UsuarioDto.java
@@ -0,0 +1,7 @@
+package br.com.alura.codechella.infra.controller;
+
+import java.time.LocalDate;
+
+public record UsuarioDto(String cpf, String nome, LocalDate nascimento, String email) {
+
+}
