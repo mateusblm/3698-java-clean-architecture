b8d9564b7cbf4d8ec58653803a603693398745ca
Mateus Burlamaqui Moreira <mateusblm@outlook.com>
2025-09-22 15:12:44 -0300

    feat: implementa repositório e mapeamento de entidade para usuário

---
+++ src/main/java/br/com/alura/codechella/infra/gateways/RepositorioDeUsuarioJpa.java
@@ -0,0 +1,30 @@
+package br.com.alura.codechella.infra.gateways;
+
+import java.util.List;
+
+import br.com.alura.codechella.application.gateways.RepositorioDeUsuario;
+import br.com.alura.codechella.domain.entities.usuario.Usuario;
+import br.com.alura.codechella.infra.persistence.UsuarioEntity;
+import br.com.alura.codechella.infra.persistence.UsuarioRepository;
+
+public class RepositorioDeUsuarioJpa implements RepositorioDeUsuario{
+    
+    private final UsuarioRepository usuarioRepository;
+
+    private final UsuarioEntityMapper mapper;
+    
+    public RepositorioDeUsuarioJpa(UsuarioRepository usuarioRepository, UsuarioEntityMapper mapper) {
+        this.usuarioRepository = usuarioRepository;
+        this.mapper = mapper;
+    }
+
+    public Usuario cadastrarUsuario(Usuario usuario) {
+        UsuarioEntity usuarioEntity = mapper.toEntity(usuario);
+        usuarioRepository.save(usuarioEntity);
+        return mapper.fromEntity(usuarioEntity);
+    }
+
+    public List<Usuario> listarTodos(){
+        return null;
+    }
+}
