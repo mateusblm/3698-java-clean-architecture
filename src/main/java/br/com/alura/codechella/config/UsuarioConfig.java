6a380bfabe1f18ced547aceea0b362ecc3b00dd9
Mateus Burlamaqui Moreira <mateusblm@outlook.com>
2025-09-22 15:39:48 -0300

    feat: adicionando classe UsuarioConfig para configuração do cadastro de usuario

---
+++ src/main/java/br/com/alura/codechella/config/UsuarioConfig.java
@@ -0,0 +1,29 @@
+package br.com.alura.codechella.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.alura.codechella.application.gateways.RepositorioDeUsuario;
+import br.com.alura.codechella.application.usecases.CadastrarUsuario;
+import br.com.alura.codechella.infra.gateways.RepositorioDeUsuarioJpa;
+import br.com.alura.codechella.infra.gateways.UsuarioEntityMapper;
+import br.com.alura.codechella.infra.persistence.UsuarioRepository;
+
+@Configuration
+public class UsuarioConfig {
+
+    @Bean
+    CadastrarUsuario cadastrarUsuario(RepositorioDeUsuario repositorioDeUsuario) {
+        return new CadastrarUsuario(repositorioDeUsuario);
+    }
+
+    @Bean
+    RepositorioDeUsuarioJpa criarRepositorioDeUsuarioJpa(UsuarioRepository repositorioDeUsuario, UsuarioEntityMapper mapper) {
+        return new RepositorioDeUsuarioJpa(repositorioDeUsuario, mapper);
+    }
+
+    @Bean
+    UsuarioEntityMapper criarUsuarioEntityMapper() {
+        return new UsuarioEntityMapper();
+    }
+}
